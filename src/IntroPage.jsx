5e07a79c1a8a5f9e000df9634ba92020bea04a53
Barbora Klusackova <barboraklusackova@gmail.com>
2025-01-28 14:43:40 +1300

    add new styling for pages

--- src/IntroPage.jsx
+++ src/IntroPage.jsx
@@ -1,7 +1,15 @@
-import { Button, Heading, Text, View } from '@aws-amplify/ui-react'
+import React from 'react'
+import {
+  Button,
+  Heading,
+  Text,
+  View,
+  ThemeProvider,
+  useTheme,
+} from '@aws-amplify/ui-react'
 import { Link } from 'react-router-dom'
 
-export default function IntroPage() {
+function IntroPageContent() {
   return (
     <View padding="3rem" textAlign="center">
       <Heading level={1}>Welcome to My Bucket List App</Heading>
@@ -13,5 +21,46 @@ export default function IntroPage() {
         <Button variation="primary">Go to My Bucket List</Button>
       </Link>
     </View>
+  )
+}
+
+export default function IntroPage() {
+  const { tokens } = useTheme()
+  const theme = {
+    name: 'Intro Page Theme',
+    tokens: {
+      colors: {
+        brand: {
+          primary: {
+            10: tokens.colors.purple[10],
+            80: tokens.colors.purple[80],
+            90: tokens.colors.purple[90],
+            100: tokens.colors.purple[100],
+          },
+        },
+      },
+      components: {
+        button: {
+          primary: {
+            backgroundColor: tokens.colors.purple[80],
+            _hover: {
+              backgroundColor: tokens.colors.purple[90],
+            },
+          },
+        },
+        heading: {
+          color: tokens.colors.purple[100],
+        },
+        text: {
+          color: tokens.colors.neutral[80],
+        },
+      },
+    },
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <IntroPageContent />
+    </ThemeProvider>
   )
 }
